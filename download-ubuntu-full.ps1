da3fd64164926ae4fc3f238d8927a692df95349b
Christopher Hill <chill.signup@gmail.com>
2019-01-07 18:19:09 +0000

    initial upload

---
+++ download-ubuntu-full.ps1
@@ -0,0 +1,67 @@
+# Checks and downloads your Ubuntu isos (full versions).
+# Written by Christopher Hill 2019/07/01
+
+$ubuntu_versions = @("18.04.1","18.10","19.04")
+
+foreach ($ubuntu_version in $ubuntu_versions) 
+{
+    $ubuntu_archive = "http://releases.ubuntu.com/$ubuntu_version/MD5SUMS"
+    $md5_file_name = $ubuntu_version + ".MD5SUMS"
+
+    $statuscode = "schrodeingerstatus"
+
+    try 
+    {
+        $statuscode = (invoke-webrequest -uri $ubuntu_archive -UseBasicParsing -TimeoutSec 5 -DisableKeepAlive -method Head).statusdescription
+    }
+    Catch 
+    {
+    }
+
+    if ($statuscode -eq "OK") 
+    {
+        Write-Host " "
+		Write-Host -ForegroundColor green "URL for $ubuntu_version exists"
+        Write-Host " "
+        
+        Invoke-WebRequest -uri $ubuntu_archive -OutFile .\$md5_file_name
+        $remote_file_hashes = Get-Content -Path .\$md5_file_name
+
+        foreach ($Data in $remote_file_hashes) 
+        {
+            $Data = $Data -split(' ')
+            $hash = $Data[0]
+            $file_name = $Data[1].Trim("*")
+
+            $file_already_downloaded = Test-Path ".\$file_name"
+
+            if ($file_already_downloaded -eq "True") 
+            {
+                Write-host "$file_Name is already downloaded. Checking hash."
+
+                $current_file_hash = Get-FileHash ".\$file_name" -Algorithm MD5
+
+                if (Compare-Object -ReferenceObject $hash -DifferenceObject $current_file_hash.hash)
+                {
+                    write-host -ForegroundColor red "Hash is different. Downloading current version."
+                    Invoke-WebRequest -Uri http://releases.ubuntu.com/$ubuntu_version/$file_Name -OutFile .\$file_Name
+                }
+                else 
+                {
+                    Write-Host -ForegroundColor green "Hash is correct. You already have the latest version."
+                }
+            }
+            else 
+            { 
+                Write-host -ForegroundColor yellow "$file_name downloading for the first time"
+				Invoke-WebRequest -Uri http://releases.ubuntu.com/$ubuntu_version/$file_Name -OutFile .\$file_Name
+            }
+         }
+    }
+    else 
+    { 
+        Write-host " "
+		Write-host -ForegroundColor red "URL for Ubuntu $ubuntu_version doesn't exist"
+		Write-Host " "
+    }
+}
---
+++ download-ubuntu-mini.ps1
@@ -0,0 +1,37 @@
+# Downloads all available Ubuntu mini isos.
+# Written by Christopher Hill 2019/07/01
+# Make sure to update $distro and $ubuntu_versions with the latest info.
+
+$distros = @("bionic","cosmic","disco","echo")
+$ubuntu_versions = @("18.04.1","18.10","19.04","19.10")
+$architectures = @("i386","amd64")
+
+foreach ($distro in $distros) 
+{
+    $ubuntu_version = $ubuntu_versions[$distros.indexOf($distro)]
+    foreach ($architecture in $architectures) 
+    {
+        $ubuntu_archive = "http://archive.ubuntu.com/ubuntu/dists/$distro/main/installer-$architecture/current/images/netboot/mini.iso"
+        $ubuntu_file = $($env:USERPROFILE)+"\Downloads\Ubuntu-mini-$ubuntu_version-$architecture.iso"
+        
+        try 
+        {
+            $statuscode = (invoke-webrequest -uri $ubuntu_archive -UseBasicParsing -TimeoutSec 5 -DisableKeepAlive -method Head).statusdescription
+        }
+        catch 
+        {
+            $statuscode = "FAIL"
+        }
+        
+        if ($statuscode -eq "OK") 
+        {
+            Write-Host -ForegroundColor Green "Downloading Ubuntu-mini-$ubuntu_version-$architecture.iso to $env:USERPROFILE\Downloads\"
+            #Invoke-WebRequest $ubuntu_archive -OutFile $ubuntu_file
+            
+        }
+        else 
+        {
+            Write-Host -ForegroundColor Red "Ubuntu-mini-$ubuntu_version-$architecture.iso doesn't exist"
+        }
+    }
+}
