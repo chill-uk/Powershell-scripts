3158021dbf13d4eeb11106e31367cba1b97d4a74
Christopher Hill <chill.signup@gmail.com>
2019-01-10 11:06:36 +0000

    Update download-ubuntu-full.ps1

--- download-ubuntu-full.ps1
+++ download-ubuntu-full.ps1
@@ -25,7 +25,7 @@ function generate_hash
 
 function download_iso
 {
-	Write-Host "Downloading $ubuntu_url$file_Name" 
+    Write-Host "Downloading $ubuntu_url$file_Name" 
     Invoke-WebRequest -Uri $ubuntu_url$file_Name -OutFile .\$file_Name
     $file_downloaded = Test-Path ".\$file_name"
     if ($file_downloaded -ne "True")
@@ -36,78 +36,78 @@ function download_iso
 
 foreach ($ubuntu_version in $ubuntu_versions) 
 {
-	$ubuntu_url = "http://releases.ubuntu.com/$ubuntu_version/"
-	$ubuntu_archive = $ubuntu_url + "MD5SUMS"
-	$md5_file_name = $ubuntu_versions + ".MD5SUMS"
+    $ubuntu_url = "http://releases.ubuntu.com/$ubuntu_version/"
+    $ubuntu_archive = $ubuntu_url + "MD5SUMS"
+    $md5_file_name = $ubuntu_versions + ".MD5SUMS"
 
-	try 
-	{
-		$statuscode = (invoke-webrequest -uri $ubuntu_archive -UseBasicParsing -TimeoutSec 5 -DisableKeepAlive -method Head).statusdescription
-	}
-	catch 
-	{
-		$statuscode = "URL DOES NOT EXIST"
-	}
+    try 
+    {
+        $statuscode = (invoke-webrequest -uri $ubuntu_archive -UseBasicParsing -TimeoutSec 5 -DisableKeepAlive -method Head).statusdescription
+    }
+    catch 
+    {
+        $statuscode = "URL DOES NOT EXIST"
+    }
 
-	if ($statuscode -eq "OK") 
-	{
-		Write-Host " "
-		Write-Host -ForegroundColor green "URL for $ubuntu_version exists"
-		Write-Host " "
+    if ($statuscode -eq "OK") 
+    {
+        Write-Host " "
+        Write-Host -ForegroundColor green "URL for $ubuntu_version exists"
+        Write-Host " "
+	
+        Invoke-WebRequest -uri $ubuntu_archive -OutFile .\$md5_file_name
+        $remote_file_hashes = Get-Content -Path .\$md5_file_name
+
+        foreach ($Data in $remote_file_hashes) 
+        {
+            $Data = $Data -split(' ')
+            $hash = $Data[0]
+            $file_name = $Data[1].Trim("*")
+
+            $file_downloaded = Test-Path ".\$file_name"
+
+            if ($file_downloaded -eq "True")
+                {
+                Write-Host "$file_Name is already downloaded."
+                Write-Host -ForegroundColor yellow "Checking for hash."
 		
-		Invoke-WebRequest -uri $ubuntu_archive -OutFile .\$md5_file_name
-		$remote_file_hashes = Get-Content -Path .\$md5_file_name
-	
-		foreach ($Data in $remote_file_hashes) 
-		{
-			$Data = $Data -split(' ')
-			$hash = $Data[0]
-			$file_name = $Data[1].Trim("*")
-
-			$file_downloaded = Test-Path ".\$file_name"
-	
-			if ($file_downloaded -eq "True") 
-			{
-				Write-Host "$file_Name is already downloaded."
-				Write-Host -ForegroundColor yellow "Checking for hash."
-				$ErrorActionPreference = "silentlyContinue"
-				$current_file_hash = Get-Content ".\$file_name" -Stream FileHash
-				$ErrorActionPreference = "Continue"
-				if ($null -eq $current_file_hash)
-				{
-					Write-Host -ForegroundColor yellow "Hash not found."
-					$current_file_hash = generate_hash
-				}
-				if ($hash -ne $current_file_hash)
-				{
-					Write-Host -ForegroundColor red "Hash is different, downloading new version"
-					download_iso
-                    $current_file_hash = generate_hash
-				}
-			}
-			else 
-			{
-				Write-Host -ForegroundColor yellow "$file_name downloading for the first time"
+                $ErrorActionPreference = "silentlyContinue"
+                $current_file_hash = Get-Content ".\$file_name" -Stream FileHash
+                $ErrorActionPreference = "Continue"
+                if ($null -eq $current_file_hash)
+                        {
+                                Write-Host -ForegroundColor yellow "Hash not found."
+                                $current_file_hash = generate_hash
+                        }
+                        if ($hash -ne $current_file_hash)
+                        {
+                                Write-Host -ForegroundColor red "Hash is different, downloading new version"
+                                download_iso
+                                $current_file_hash = generate_hash
+                        }
+            }
+            else
+            {
+                Write-Host -ForegroundColor yellow "$file_name downloading for the first time"
                 download_iso
                 $current_file_hash = generate_hash
-			}
-		
-			if ($hash -ne $current_file_hash)
-			{
-				Write-Host -ForegroundColor red "$file_name verification failed." 
-				Write-Host -ForegroundColor red "Either your iso is corrupt, or Ubuntu's MD5 hashes are out of date"
-			}
-			else
-			{
-				Write-Host -ForegroundColor green "Verification successfull. You have the latest version"
-				Write-Host ""
-			}
-		}
-	}
-	else 
-	{ 
-		Write-Host " "
-		Write-Host -ForegroundColor red "URL for Ubuntu $ubuntu_version doesn't exist"
-		Write-Host " "
-	}
+            }
+            if ($hash -ne $current_file_hash)
+            {
+                Write-Host -ForegroundColor red "$file_name verification failed."
+                Write-Host -ForegroundColor red "Either your iso is corrupt, or Ubuntu's MD5 hashes are out of date"
+            }
+            else
+            {
+                Write-Host -ForegroundColor green "Verification successfull. You have the latest version"
+                Write-Host ""
+            }
+        }
+    }
+    else 
+    { 
+        Write-Host " "
+        Write-Host -ForegroundColor red "URL for Ubuntu $ubuntu_version doesn't exist"
+        Write-Host " "
+    }
 }
